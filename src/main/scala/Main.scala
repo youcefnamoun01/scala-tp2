728ccae3ca6e3b35fbbf839443e8b9c1e9e188ef
Youcef NAMOUN <youcef.na@kuikops.com>
2025-08-18 13:26:39 +0200

    quick fix

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,29 @@
 object Main {
   def main(args: Array[String]): Unit = {
+
     // Initialiser le schéma en DB
     DatabaseManager.initSchema()
 
     println("\n AJOUT DES DOCUMENTS:")
-    val book1 = new Book("Le Petit Prince", "Antoine de Saint-Exupéry", 1943, "Fiction")
-    val book2 = new Book("1984", "George Orwell", 1949, "Science-fiction")
-    val magazine1 = new Magazine("Science & Vie", "Équipe éditoriale", 2024, 1258)
-    val comic1 = new Comic("Astérix", "René Goscinny & Albert Uderzo", 1961, 1)
-    val comic2 = new Comic("Tintin", "Hergé", 1929, 12)
+    val book1 = new Book("Harry Potter", "J.K. Rowling", 1997, "Fantasy")
+    val book2 = new Book("Le Seigneur des Anneaux", "J.R.R. Tolkien", 1954, "Fantasy")
+    val magazine1 = new Magazine("Science & Vie", "Equipe éditoriale", 2024, 1258)
+    val magazine2 = new Magazine("National Geographic", "Redaction NG", 2023, 789)
+    val comic1 = new Comic("Asterix", "Rene Goscinny & Albert Uderzo", 1961, 1)
+    val comic2 = new Comic("Tintin", "Herge", 1929, 12)
 
-    // Ajout en DB uniquement
+    // Ajout en DB
     val book1Id = DatabaseManager.insertDocument(Mapper.toEntity(book1))
     val book2Id = DatabaseManager.insertDocument(Mapper.toEntity(book2))
     val magazine1Id = DatabaseManager.insertDocument(Mapper.toEntity(magazine1))
+    val magazine2Id = DatabaseManager.insertDocument(Mapper.toEntity(magazine2))
     val comic1Id = DatabaseManager.insertDocument(Mapper.toEntity(comic1))
     val comic2Id = DatabaseManager.insertDocument(Mapper.toEntity(comic2))
 
     println("\n AJOUT DES UTILISATEURS:")
-    val user1 = new User("Alice")
-    val user2 = new User("Bob")
-    val user3 = new User("Charlie")
+    val user1 = new User("Youcef")
+    val user2 = new User("Juba")
+    val user3 = new User("Omar")
 
     DatabaseManager.insertUser(UserEntity(None, user1.name))
     DatabaseManager.insertUser(UserEntity(None, user2.name))
